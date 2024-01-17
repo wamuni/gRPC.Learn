1b3ebed18286e04627744fbddeb81102e1f4fbfc
wamuni <boshi.pan@outlook.com>
2024-01-17 16:04:37 +0000

    server side code

--- hello-server/main.go
+++ hello-server/main.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"net"
+
+	"google.golang.org/grpc"
+	pb "wamuni.cc/grpc/implementation/hello-server/proto"
+)
+
+type server struct {
+	pb.UnimplementedSayHelloServer
+}
+
+func (s *server) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloReponse, error) {
+	return &pb.HelloReponse{ResponseMsg: "Hello" + req.RequestName}, nil
+}
+
+func main() {
+	listen, _ := net.Listen("tcp", ":9090")
+	grpcServer := grpc.NewServer()
+	pb.RegisterSayHelloServer(grpcServer, &server{})
+	err := grpcServer.Serve(listen)
+
+	if err != nil {
+		fmt.Printf("failed to server: %v", err)
+	}
+}
