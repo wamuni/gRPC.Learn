da8a0cb3b3b555da1c11fc53bba6262975df0b05
wamuni <boshi.pan@outlook.com>
2024-01-17 17:16:12 +0000

    update format of the info in hello-client/main.go

--- hello-client/main.go
+++ hello-client/main.go
@@ -10,12 +10,12 @@ import (
 )
 
 func main() {
-	conn, err := grpc.Dial("127.0.0.0:9090", grpc.WithTransportCredentials(insecure.NewBundle().TransportCredentials()))
+	conn, err := grpc.Dial("127.0.0.1:9090", grpc.WithTransportCredentials(insecure.NewBundle().TransportCredentials()))
 	if err != nil {
 		fmt.Printf("did not connect: %v", err)
 	}
 	defer conn.Close()
 	client := pb.NewSayHelloClient(conn)
 	message, _ := client.SayHello(context.Background(), &pb.HelloRequest{RequestName: "Eddie"})
-	fmt.Printf("Message from gRPC server: %v", message)
+	fmt.Printf("Message from gRPC server: %v\n", message)
 }
